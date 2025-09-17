426dcdb50988a536b62afba503e5fcfbb2c06963
Lee HunYoung <2hy0419@naver.com>
2025-09-17 22:06:54 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        
+        int m_length = message.length();
+        answer = m_length * 2;
+        
+        return answer;
+    }
+}
