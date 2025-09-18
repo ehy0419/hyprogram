60ef78483c5982aa8fbbc1f08d55f25f86a4395b
Lee HunYoung <2hy0419@naver.com>
2025-09-18 19:38:52 +0900

    [level 0] Title: 문자열 출력하기, Time: 179.04 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // Scanner 객체 생성
+        String str = sc.nextLine();          // 입력받은 문자열 저장
+        System.out.println(str);             // 문자열 출력
+    }
+}
