39120e53bdd439e07db0e1fe03827763b9a5613a
Lee HunYoung <2hy0419@naver.com>
2025-10-17 08:56:43 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.03 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int mul = 1;
+        int sum = 0;
+
+        for (int num : num_list) {
+            mul *= num;
+            sum += num;
+        }
+
+        if (mul < sum * sum) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
